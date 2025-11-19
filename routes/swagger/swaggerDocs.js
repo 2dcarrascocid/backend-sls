6f2d83317ec2d32f134c4d48d0e121d8fa2a36a9
2dcarrasco <2dcarrascocid@gmail.com>
2025-11-19 17:04:05 -0300

    swagger ok

--- routes/swagger/swaggerDocs.js
+++ routes/swagger/swaggerDocs.js
@@ -1,40 +1,55 @@
-import swaggerJSDoc from "swagger-jsdoc";
+import pkg from "swagger-jsdoc";
+const swaggerJSDoc = pkg.default || pkg;
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API Fair Play Chile",
+      version: "1.0.0",
+    },
+  },
+  apis: ["./routes/**/*.js"],
+};
 
 export const handler = async () => {
-  const options = {
-    definition: {
-      openapi: "3.0.0",
-      info: { title: "API Fair Play Chile", version: "1.0.0" }
-    },
-    apis: ["routes/**/*.js"]
+  try {
+    console.log("Generando Swagger spec...");
+    const spec = swaggerJSDoc(options);
 
+    const html = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Swagger UI</title>
+        <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
+      </head>
+      <body>
+        <div id="swagger"></div>
+        <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+        <script>
+          SwaggerUIBundle({
+            spec: ${JSON.stringify(spec)},
+            dom_id: '#swagger'
+          });
+        </script>
+      </body>
+    </html>`;
 
-  };
-
-  const spec = swaggerJSDoc(options);
-
-  const html = `
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <title>Swagger UI</title>
-      <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
-    </head>
-    <body>
-      <div id="swagger"></div>
-      <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
-      <script>
-        SwaggerUIBundle({
-          spec: ${JSON.stringify(spec)},
-          dom_id: '#swagger'
-        });
-      </script>
-    </body>
-  </html>`;
-
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/html" },
-    body: html
-  };
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "text/html" },
+      body: html,
+    };
+  } catch (err) {
+    console.error("ERROR SWAGGER:", err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Swagger error",
+        error: err.message,
+        stack: err.stack,
+      }),
+    };
+  }
 };
